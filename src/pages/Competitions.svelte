bd083ede7c6fbc4c0358b192056afb0c0928cef3
Liam S. Crouch <liam@phoenixlan.no>
2023-08-26 23:18:51 +0900

    Fix up the competitions list

--- src/pages/Competitions.svelte
+++ src/pages/Competitions.svelte
@@ -10,34 +10,28 @@ import PageHeader from "../components/PageHeader.svelte";
         <div class="content-inner">
             <p>Er det konkurranser du savner? Si ifra til oss på Discord, eller søk Game crew og bli med på å arrangere det selv!</p>
             <h2>Valorant</h2>
-            <p><a href="https://forms.gle/1UnhLSgG8KQu66Wt5">Påmelding(Lagledere)</a></p>
-            <p><a href="https://forms.gle/krmoYmDxkvsSwvdt8">Påmelding(Lagmedlem)</a></p>
             <ul>
                 <li>Standard with OT</li>
                 <li>No draws</li>
                 <li>Up to 13 wins and/or 26 rounds(30-45 min)</li>
             </ul>
             <h2>Minecraft Battle Royale</h2>
-            <p><a href="https://forms.gle/JDjkPyEUYCfKR4bP6">Påmelding</a></p>
             <p>Alle mot alle, Hunger Games style. Tre runder vil kjøres totalt. Total poengsum fra de tre rundene vil bestemme vinner, så lurt å slenge seg med på alt!</p>
             <h2>Phoenix Datasnok</h2>
             <p><b>Påmelding skjer via konkurransesystemet på LANet</b></p>
             <p>Er <b>DU</b> god på data? Vis dine ferdigheter ved å delta på Datasnok, en "hacke-rebus" du vinner ved å løse oppgaver av forskjellig vanskelighetsgrad.</p>
+            <p>Konkurransetypene varierer fra "Finn det hemmelige passordet" til "Finn QR-koder gjemt på bygget" til "Finn sikkerhetshull i svak kode"</p>
             <ul>
                 <li>Ingen teaming</li>
                 <li>Flest poeng vinner</li>
                 <li>Førstemann som fikk poengsummen blir valgt dersom det blir likt</li>
             </ul>
-            <p><a href="https://datasnok.phoenixlan.no">datasnok.phoenixlan.no</a></p>
 			<h2>Minecraft Build Battle</h2>
-            <p><a href="https://forms.gle/MPNisMkfpZahe9379">Påmelding</a></p>
-            <p>Er du mer interessert i bygging enn deathmatch?  Bygg i det frie gjennom LANet, og få sjansen til å vinne premie! En jury fra Phoenix Crew vil gå gjennom alle innslagene og gi vurderinger. Konkurransen starter så snart vi åpner, og du kan bygge frem til søn. Kl 11. </p>
+            <p>Er du mer interessert i bygging enn deathmatch?  Bygg i det frie gjennom LANet, og få sjansen til å vinne premie! En jury fra Phoenix Crew vil gå gjennom alle innslagene og gi vurderinger. Konkurransen starter så snart vi åpner, og du kan bygge frem til søn. kl 11. </p>
             <h2>Beatsaber:</h2>
-            <p>LAN’et har endelig VR! Den som greier høyeste high score i Beatsaber gjennom LANet får muligheten til å vinne premie. En bra mulighet til å teste spennende ny form for gaming uten å straffe lommeboka for alle som vil!</p>
+            <p>Høyeste score på et sett med forhåndsbestemte sanger vinner. Du har hele LANet til å prøve deg.</p>
             <h2>Småkonkurranser</h2>
-            <h3>Paint</h3>
-            <p><a href="https://forms.gle/utfwW9sPs1yiA22R7">Påmelding</a></p>
-            <p>Ønsker du å tegne mellom matches? Lag et fint bilde gjennom LANet, og send til oss! En jury fra Phoenix Crew vil gå gjennom alle innslagene og gi vurderinger. Konkurransen starter så snart vi åpner, og du kan bygge frem til søn. Kl 11.</p>
+            <p>I løpet av LANet arrangerer vi småkonkurranser i alt fra bordtennis til </p>
         </div>
     </div>
 </div>
@@ -45,14 +39,27 @@ import PageHeader from "../components/PageHeader.svelte";
 p {
     max-width: 60em;
 }
+
+h2 {
+    display: inline-block;
+    margin-bottom: 0;
+}
+
+h2:after {
+    content: " ";
+    display: block;
+    border: 2px solid #f451a0;
+    width: 100%;
+    margin-top: 0.4em;
+}
 .content {
     padding: 1rem;
     background-color: #111111;
     display: flex;
     justify-content: center;
 }
-.center {
-  text-align: center;
+.content-inner{
+  text-align: left;
   list-style-position: inside;
 }
 </style>
