eb5fd69185233f1a5ff1e1702ab8a55ee659a4de
MLJ <mlj@pop-os.localdomain>
2023-09-22 13:35:27 +0200

    added compo signup links

--- src/pages/Competitions.svelte
+++ src/pages/Competitions.svelte
@@ -15,8 +15,14 @@ import PageHeader from "../components/PageHeader.svelte";
                 <li>No draws</li>
                 <li>Up to 13 wins and/or 26 rounds(30-45 min)</li>
             </ul>
+            <h3> <ahref="https://forms.gle/tgExMg9vdZysHCKi8">Påmeldingskjema Lagleder </a></h3>
+            <h3> <ahref="https://forms.gle/2UvadeokqLfBTQYj6">Påmeldingskjema Lagkamerat </a></h3>
             <h2>Minecraft Battle Royale</h2>
             <p>Alle mot alle, Hunger Games style. Tre runder vil kjøres totalt. Total poengsum fra de tre rundene vil bestemme vinner, så lurt å slenge seg med på alt!</p>
+            <h3> <ahref="https://forms.gle/T8WUzMx2XnLkXQJVA">Påmeldingskjema MC:Battle Royale </a></h3>
+            <h2>Minecraft Build Battle</h2>
+            <p>Er du mer interessert i bygging enn deathmatch?  Bygg i det frie gjennom LANet, og få sjansen til å vinne premie! En jury fra Phoenix Crew vil gå gjennom alle innslagene og gi vurderinger. Konkurransen starter så snart vi åpner, og du kan bygge frem til søn. kl 11. </p>
+            <h3> <ahref="https://forms.gle/rxKEKQPK4ybqXk3PA">Påmeldingskjema MC:Build Battle </a></h3>
             <h2>Phoenix Datasnok</h2>
             <p><b>Påmelding skjer via konkurransesystemet på LANet</b></p>
             <p>Er <b>DU</b> god på data? Vis dine ferdigheter ved å delta på Datasnok, en "hacke-rebus" du vinner ved å løse oppgaver av forskjellig vanskelighetsgrad.</p>
@@ -25,13 +31,15 @@ import PageHeader from "../components/PageHeader.svelte";
                 <li>Ingen teaming</li>
                 <li>Flest poeng vinner</li>
                 <li>Førstemann som fikk poengsummen blir valgt dersom det blir likt</li>
-            </ul>
-			<h2>Minecraft Build Battle</h2>
-            <p>Er du mer interessert i bygging enn deathmatch?  Bygg i det frie gjennom LANet, og få sjansen til å vinne premie! En jury fra Phoenix Crew vil gå gjennom alle innslagene og gi vurderinger. Konkurransen starter så snart vi åpner, og du kan bygge frem til søn. kl 11. </p>
+            </ul>		
             <h2>Beatsaber:</h2>
             <p>Høyeste score på et sett med forhåndsbestemte sanger vinner. Du har hele LANet til å prøve deg.</p>
             <h2>Småkonkurranser</h2>
             <p>I løpet av LANet arrangerer vi småkonkurranser i alt fra bordtennis til Kahoot og retro-arkademaskin</p>
+            <h2>CS: Wingman</h2>
+            <p>2v2 dueller i CC:GO competitive mode. Lagkaptein fyller ut skjema for hele duoen under.</p>
+            <h3> <ahref="https://forms.gle/MZjzedqTywsiVzev7">Påmeldingskjema CS:Wingman </a></h3>
+            
         </div>
     </div>
 </div>
